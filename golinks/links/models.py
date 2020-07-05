b4e368a99ff980bd8b6d096e12dcd9de27ed5857
Wesley Soo-Hoo <wesoohoo@gmail.com>
2020-07-05 11:36:01 -0700

    /short_link/set works and user permissions created

--- golinks/links/models.py
+++ golinks/links/models.py
@@ -5,7 +5,8 @@ from django.contrib.auth.models import User
 class Link(models.Model):
     long_link = models.CharField(max_length=200)
     short_link = models.CharField(max_length=200, unique=True)
-    owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True,
+                              blank=True)
 
     def __str__(self):
         return self.short_link
--- golinks/links/views.py
+++ golinks/links/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.core.exceptions import PermissionDenied
 
 from .models import Link
 
 # Create your views here.
 def index(request):
     context = {
-        'link_list': Link.objects.all(),
+        'link_list': Link.objects.all().order_by('short_link'),
     }
     return render(request, "links/index.html", context)
 
@@ -21,10 +23,41 @@ def forward(request, short_link):
 
 
 def edit(request, short_link):
-    return HttpResponse(short_link)
-    # return render(request, 'links/edit.html')
+    context = {
+        'short_link': short_link,
+    }
+    return render(request, 'links/edit.html', context)
 
 
-def set_link(requets, short_link):
+def set_link(request, short_link):
+    if 'long_link' not in request.POST.keys():
+        return HttpResponse("No link provided")
+
+    user = None  # null user means public link (editable by anyone)
+    if request.user.is_authenticated:
+        user = request.user
+
+    # By default, links are set to public. If the 'anonymous' tag in the POST
+    # data is set to anything except for 'true', it will become a private link.
+    # Private links are defined as visible to everyone but only editable by the
+    # user who created it
+    anonymous = True
+    if 'anonymous' in request.POST.keys() and request.user.is_authenticated:
+        anonymous = (request.POST['anonymous'] == 'true')
+    print(anonymous)
+
+    long_link = request.POST['long_link']
+    try:
+        link = Link.objects.get(short_link=short_link)
+        # Must have permissions
+        if link.owner is None or link.owner == user:
+            link.long_link = long_link
+            link.save()
+        else:
+            raise PermissionDenied
+    except Link.DoesNotExist:
+        link = Link(short_link=short_link, long_link=long_link, owner=user)
+        link.save()
+
     return HttpResponse(short_link)
 
