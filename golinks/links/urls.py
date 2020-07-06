494c1876e8704e82e3be6645c5e034af875702c4
Wesley Soo-Hoo <wesoohoo@gmail.com>
2020-07-06 16:04:24 -0700

    fix trailing / bug

--- golinks/links/urls.py
+++ golinks/links/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('<str:short_link>/edit', views.edit_view, name='edit'),
     path('<str:short_link>/set', views.set_link_view, name='set'),
     path('<str:short_link>', views.forward_view, name='forward'),
+    path('<str:short_link>/', views.forward_view, name='forward'),
     path('int/login', views.login_view, name='login'),
     path('int/logout', views.logout_view, name='logout'),
     path('int/auth', views.auth_view, name='auth'),
